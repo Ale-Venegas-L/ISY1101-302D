5bb8972a6260918357e2e814d28576b3ceadde7e
Ale-Venegas-L <aleja.venegas@duocuc.cl>
2026-04-01 09:47:30 -0300

    Adición de archivos

---
+++ main.tf
@@ -0,0 +1,15 @@
+// vpc con subred publica y privada
+
+// security groups & nat
+
+//ec2-1/front: docker y ngix
+
+//ec2-2/back: docker y microservicio, solo acceso de front
+
+//ec2-3/data: db relacional (mysql o PostgreSQL), solo acceso de back
+
+//instalar y verificar docker, git y db
+
+//automatizar despliegue
+
+//evidenciar conectividad
